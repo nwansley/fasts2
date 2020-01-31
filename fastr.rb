6b4df469fae593a9d94543d644b13dbc1c6c9af7
nwansley <44908595+nwansley@users.noreply.github.com>
2020-01-31 13:48:00 -0800

    Add files via upload

---
+++ fastr.rb
@@ -0,0 +1,91 @@
+use_bpm 80
+use_synth :piano
+
+intro = "C:/Users/nicholas_wansley/Downloads/tokyo.wav"
+ad = "C:/Users/nicholas_wansley/Downloads/ad (online-audio-converter.com).wav"
+gd = "C:/Users/nicholas_wansley/Documents/Audacity/gd.wav"
+x = 1
+sample intro
+
+sleep 46
+
+live_loop:piano do
+  2.times do
+    play :fb5,amp: x
+    sleep 0.5
+    play :cb7,amp: x
+    sleep 0.5
+    play :bb6,amp: x
+    sleep 0.5
+    play :fb5,amp: x,release: 2
+    sleep 1
+    play :bb6,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :cb7,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :db7,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :cb7,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :bb6,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :cb7,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :bb5,amp: x
+    sleep 0.5
+    x = x - 0.03
+    play :cb7,amp: x
+    sleep 0.5
+    x = x - 0.03
+    play :bb6,amp: x
+    sleep 0.5
+    x = x - 0.03
+    play :fb5,amp: x,release: 2
+    sleep 1
+    x = x - 0.03
+    play :bb6,amp: 0.6
+    sleep 0.25
+    x = x - 0.03
+    play :cb7,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :db7,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :cb7,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :bb6,amp: x
+    sleep 0.25
+    x = x - 0.03
+    play :cb7,amp: x
+    sleep 0.25
+    x = x - 0.03
+  end
+  stop
+end
+sleep 5
+
+live_loop:ad do
+  3.times do
+    sample ad
+    sleep 5
+  end
+  stop
+end
+
+sleep 12
+
+sample gd
+
+
+
+
+
+
